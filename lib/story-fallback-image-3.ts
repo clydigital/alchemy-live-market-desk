6933ed7b8808b609305bc95dc7216117fa6eb6a5
clydigital <leeyanghere@gmail.com>
2026-08-07 10:21:05 +0800

    Assemble Story fallback artwork 3

---
+++ lib/story-fallback-image-3.ts
@@ -0,0 +1,6 @@
+import { STORY_FALLBACK_IMAGE_3P } from "./story-fallback-image-3p";
+import { STORY_FALLBACK_IMAGE_3A } from "./story-fallback-image-3a";
+import { STORY_FALLBACK_IMAGE_3B } from "./story-fallback-image-3b";
+import { STORY_FALLBACK_IMAGE_3C } from "./story-fallback-image-3c";
+
+export const STORY_FALLBACK_IMAGE_3 = `data:image/webp;base64,${STORY_FALLBACK_IMAGE_3P}${STORY_FALLBACK_IMAGE_3A}${STORY_FALLBACK_IMAGE_3B}${STORY_FALLBACK_IMAGE_3C}`;
